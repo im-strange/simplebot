8b09e2eb99f15669f3253a56c5faafbb3e65c494
im-strange <itsmestrange.dump@gmail.com>
2023-05-07 19:12:58 +0800

    setup.py added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+  name="simplebot",
+  version="1.0",
+  url="https://github.com/im-strange/simplebot.git",
+  author="Samuel Genoguin",
+  packages=["simplebot"]
+)
