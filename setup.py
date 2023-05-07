3ff4ee38d4c80f99d6d56729489e0c6a81da2911
im-strange <itsmestrange.dump@gmail.com>
2023-05-07 19:15:11 +0800

    setup.py updated

--- setup.py
+++ setup.py
@@ -4,6 +4,5 @@ setup(
   name="simplebot",
   version="1.0",
   url="https://github.com/im-strange/simplebot.git",
-  author="Samuel Genoguin",
-  packages=["simplebot"]
+  author="Samuel Genoguin"
 )
