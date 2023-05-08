7a57d18d5e230efc4b143c1f665aa965a82ad260
im-strange <itsmestrange.dump@gmail.com>
2023-05-08 11:36:48 +0800

    setup.py modifies

--- setup.py
+++ setup.py
@@ -6,7 +6,11 @@ setup(
   url="https://github.com/im-strange/simplebot.git",
   author="Samuel Genoguin",
   description="Module for your simple chatbot.",
+  include_package_data=True,
   packages=[
     "simplebot"
-  ]
+  ],
+  package_data={
+    "simplebot": ["data/*.json"]
+  }
 )
