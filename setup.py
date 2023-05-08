939ea9633aaa4994887019d9e64e11a3e013fd46
im-strange <itsmestrange.dump@gmail.com>
2023-05-08 10:41:49 +0800

    Modified Files

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(
   version="1.0.0",
   url="https://github.com/im-strange/simplebot.git",
   author="Samuel Genoguin",
+  description="Module for your own simple chatbot.",
   packages=find_packages(),
+  include_package_data=True,
   package_data={
     "": ["data/sample-intents.json"]
   }
