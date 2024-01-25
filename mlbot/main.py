5a79efb19274562f7b94b6b2f0cbec90cbf2ea13
im-strange <itsmestrange.dump@gmail.com>
2024-01-25 19:13:52 +0800

    update

---
+++ mlbot/main.py
@@ -0,0 +1,18 @@
+
+import csv
+import re
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+class bot:
+    def __init__(self):
+        pass
+
+    def train(self, data):
+        pass
+
+    def respond(self, text):
+        pass
